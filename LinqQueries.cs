0afd744cfb82263bf02d9ff1c9c9d7bffac3b463
Seom1177 <cesarleonardo88@hotmail.com>
2023-01-04 18:21:06 -0500

    Importando Datos

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -1,5 +1,15 @@
+using System.Text.Json;
 public class LinqQueries{
+    private List<Book> librosCollection = new List<Book>();
     public LinqQueries(){
-        
+        using(StreamReader reader = new StreamReader("books.json")){
+            string json = reader.ReadToEnd();
+            this.librosCollection = JsonSerializer.Deserialize<List<Book>>(json, new JsonSerializerOptions(){
+                PropertyNameCaseInsensitive = true
+            })!;
+        }
+    }
+    public IEnumerable<Book> TodaLaColeccion(){
+        return librosCollection;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LinqQueries queries = new LinqQueries();
+
+ImprimirValores(queries.TodaLaColeccion());
+
+void ImprimirValores (IEnumerable<Book> listadeLibros){
+    Console.WriteLine("{0, -60} {1, 15} {2, 17}\n", "Titulo", "N. Paginas", "Fecha publicacion");
+    foreach(var item in listadeLibros){
+        Console.WriteLine("{0, -60} {1, 15} {2, 17}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+    }
+}
